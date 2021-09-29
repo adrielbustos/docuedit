d09b004f13ed85ebe6fee45b8b41dc8bf0e6b13b
Adriel Bustos <adriel.bustos01@gmail.com>
2021-09-29 12:51:49 -0300

    express

--- index.js
+++ index.js
@@ -1,62 +1,72 @@
+const express = require('express');
+const app = express();
 const open = require('open');
 const fs = require('fs');
 const http = require('http');
 
-const url = "http://c1741193.ferozo.com/patroncito/public/downloads/cv.docx";
-const tempName = "temp.docx";
+app.get('/', function (req, res) {
+    const url = "http://c1741193.ferozo.com/patroncito/public/downloads/cv.docx";
+    const tempName = "temp.docx";
 
-const openFile = async (file) => {
-    await open(file, { wait: true });
-}
+    const openFile = async (file) => {
+        await open(file, { wait: true });
+    }
 
-const startWatch = (tempName) => {
-    let fsWait = false;
-    console.log(`Watching for file changes on ${tempName}`);
-    return fs.watch(`./${tempName}`, { interval: 1000 }, (event, filename) => {
-        //console.log(event);
-        if (filename) {
-            if (fsWait) return;
-            fsWait = setTimeout(() => {
-                fsWait = false;
-            }, 100);
+    const startWatch = (tempName) => {
+        let fsWait = false;
+        console.log(`Watching for file changes on ${tempName}`);
+        return fs.watch(`./${tempName}`, { interval: 1000 }, (event, filename) => {
+            //console.log(event);
+            if (filename) {
+                if (fsWait) return;
+                fsWait = setTimeout(() => {
+                    fsWait = false;
+                }, 100);
 
-            // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER
+                // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER
 
-            console.log(`${filename} file Changed`);
-        }
-    });
-}
-
-try {
-    http.get(url, function (response) {
-        const file = fs.createWriteStream(tempName);
-        response.pipe(file);
-        file.on("finish", function () {
-            file.close();
-            const watcher = startWatch(tempName);
-            openFile(tempName).then(() => {
-                console.log("file closed");
-    
-                // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER ANTES DE BORRARLO
-    
-                fs.unlinkSync(`./${tempName}`, function (erro) // cuando entra aca?
-                {
-                    console.log('delete');
-                    if (erro) throw err;
-                    // if no error, file has been deleted successfully
-                    console.log('File deleted!');
-                    process.exit();
-                });
-                watcher.close();
-            });
-            
-        })
-        file.on("error", function (err) {
-            console.log("Error to open stream the file");
+                console.log(`${filename} file Changed`);
+            }
         });
-    }).on("error", function (e) {
+    }
+
+    try {
+        http.get(url, function (response) {
+            const file = fs.createWriteStream(tempName);
+            response.pipe(file);
+            file.on("finish", function () {
+                file.close();
+                const watcher = startWatch(tempName);
+                openFile(tempName).then(() => {
+                    console.log("file closed");
+
+                    // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER ANTES DE BORRARLO
+
+                    fs.unlinkSync(`./${tempName}`, function (erro) // cuando entra aca?
+                    {
+                        console.log('delete');
+                        if (erro) throw err;
+                        // if no error, file has been deleted successfully
+                        console.log('File deleted!');
+                        process.exit();
+                    });
+                    watcher.close();
+                });
+
+            })
+            file.on("error", function (err) {
+                console.log("Error to open stream the file");
+            });
+        }).on("error", function (e) {
+            console.log("error to download");
+        });
+    } catch (error) {
         console.log("error to download");
-    });
-} catch (error) {
-    console.log("errp to download");
-}
+    }
+});
+
+app.get('*', function (req, res) {
+    res.send('404 Not Found');
+});
+
+app.listen(3000);

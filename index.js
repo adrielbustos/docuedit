236c72e50498081d14f4c03719840a5b6f09a9e9
Adriel Bustos <adriel.bustos01@gmail.com>
2021-10-12 11:19:53 -0300

    electron view files changes

--- index.js
+++ index.js
@@ -1,60 +1,13 @@
-const { app, BrowserWindow, Menu } = require("electron");
-const url = require("url");
+require("dotenv").config();
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const path = require("path");
-
 const open = require('open');
+const url = require("url");
 const fs = require('fs');
 const http = require('http');
 
 const filewUrl = "http://c1741193.ferozo.com/patroncito/public/downloads/cv.docx"; // descargar  desde s3 de amazon
 const tempName = "cv2.docx";
-
-let templateMenu = [
-    {
-        label: "File",
-        submenu: [
-            {
-                label: "test",
-                click() {
-                    createNewWindow()
-                }
-            },
-            {
-                label: "Load Docx",
-                click() {
-                    http.get(filewUrl, function (response) {
-                        const file = fs.createWriteStream(tempName);
-                        response.pipe(file);
-                        file.on("finish", function () {
-                            file.close();
-                            const watcher = startWatch(tempName);
-                            openFile(tempName).then(() => {
-                                // app.post('/upload', function (req, res) {
-                                //     console.log(req.files.foo); // the uploaded file object
-                                //     console.log("file closed");
-                                //     // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER ANTES DE BORRARLO
-                                //     fs.unlinkSync(`./${tempName}`);
-                                //     watcher.close();
-                                // });
-                                console.log("file closed");
-                                // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER ANTES DE BORRARLO
-                                fs.unlinkSync(`./${tempName}`);
-                                watcher.close();
-                            });
-                        })
-                        file.on("error", function (err) {
-                            console.log("Error to open stream the file");
-                        });
-                    }).on("error", function (e) {
-                        console.log("error to download");
-                    });
-                }
-            }
-        ]
-    }
-];
-
-let mainWindow;
 
 /**
  * Functions
@@ -106,18 +59,112 @@ const createNewWindow = () => {
  * App manage
  */
 
+let templateMenu = [
+    {
+        label: "File",
+        submenu: [
+            {
+                label: "test",
+                click() {
+                    createNewWindow()
+                }
+            },
+            {
+                label: "Load Docx",
+                click() {
+                    http.get(filewUrl, function (response) {
+                        const file = fs.createWriteStream(tempName);
+                        response.pipe(file);
+                        file.on("finish", function () {
+                            file.close();
+                            const watcher = startWatch(tempName);
+                            openFile(tempName).then(() => {
+                                // app.post('/upload', function (req, res) {
+                                //     console.log(req.files.foo); // the uploaded file object
+                                //     console.log("file closed");
+                                //     // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER ANTES DE BORRARLO
+                                //     fs.unlinkSync(`./${tempName}`);
+                                //     watcher.close();
+                                // });
+                                console.log("file closed");
+                                // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER ANTES DE BORRARLO
+                                fs.unlinkSync(`./${tempName}`);
+                                watcher.close();
+                            });
+                        })
+                        file.on("error", function (err) {
+                            console.log("Error to open stream the file");
+                        });
+                    }).on("error", function (e) {
+                        console.log("error to download");
+                    });
+                }
+            }
+        ]
+    }
+];
+
+let mainWindow;
+
+const io = require('socket.io-client');
+const socket = io(`http://localhost:8080`);
+
 app.on("ready", () => {
-    mainWindow = new BrowserWindow({});
+    
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
+    });
+
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, "views/index.html"),
         protocol: "file",
         slashes: true
     }));
+
+    mainWindow.loadFile(path.join(__dirname, "views/index.html"));
+
     const mainMenu = Menu.buildFromTemplate(templateMenu);
+
     Menu.setApplicationMenu(mainMenu);
     mainWindow.on("closed", () => {
         app.quit();
     });
+
+    mainWindow.webContents.openDevTools();
+
+    let isSocketOn = false;
+
+    socket.on('welcome', () => {
+        console.log('on welcome : welcome received renderer');
+        socket.emit('test')
+    });
+
+    socket.on('error', (e) => {
+        console.log(e);
+    });
+
+    socket.on('ok', () => {
+        console.log("OK received renderer");
+    });
+
+    socket.on('connect', function(d) {
+        isSocketOn = true;
+        console.log("conexion exitosa");
+        // socket.emit('test');
+    });
+
+    socket.on('disconnect', () => {
+        console.log("Desconectado");
+    });
+
+    ipcMain.on("loaded", (event, data) => {
+        event.reply("isConnected", isSocketOn);
+    });
+
 });
 
 if (process.env.NODE_ENV !== 'production') {

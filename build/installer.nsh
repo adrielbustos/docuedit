f5471b3f381095bf6d964d305fe7ff7c170ac001
Adriel Bustos <adriel.bustos01@gmail.com>
2021-10-25 18:05:40 -0300

    installer and post file test

--- build/installer.nsh
+++ build/installer.nsh
@@ -1,10 +1,15 @@
 !macro customInstall
-  DetailPrint "Register docuedti URI Handler"
-  DeleteRegKey HKCR "docuedti"
-  WriteRegStr HKCR "docuedti" "" "URL:docuedti"
-  WriteRegStr HKCR "docuedti" "URL Protocol" ""
-  WriteRegStr HKCR "docuedti\DefaultIcon" "" "$INSTDIR\${APP_EXECUTABLE_FILENAME}"
-  WriteRegStr HKCR "docuedti\shell" "" ""
-  WriteRegStr HKCR "docuedti\shell\Open" "" ""
-  WriteRegStr HKCR "docuedti\shell\Open\command" "" "$INSTDIR\${APP_EXECUTABLE_FILENAME} %1"
+  DetailPrint "Register docuedit URI Handler"
+  DeleteRegKey HKCR "docuedit"
+  WriteRegStr HKCR "docuedit" "" "URL:docuedit"
+  WriteRegStr HKCR "docuedit" "URL Protocol" ""
+  WriteRegStr HKCR "docuedit\DefaultIcon" "" "$INSTDIR\${APP_EXECUTABLE_FILENAME}"
+  WriteRegStr HKCR "docuedit\shell" "" ""
+  WriteRegStr HKCR "docuedit\shell\Open" "" ""
+  WriteRegStr HKCR "docuedit\shell\Open\command" "" "$INSTDIR\${APP_EXECUTABLE_FILENAME} %1"
+!macroend
+
+!macro customUnInstall
+  DetailPrint "Remove docuedit URI Handler"
+  DeleteRegKey HKCR "docuedit"
 !macroend
--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 require("dotenv").config();
 const { app, BrowserWindow, Menu, ipcMain, protocol } = require("electron");
+const gotTheLock = app.requestSingleInstanceLock();
+const io = require('socket.io-client');
+const socket = io(`http://localhost:8080`);
 const path = require("path");
 const open = require('open');
 const url = require("url");
 const fs = require('fs');
-const http = require('http');
+const http = require('https');
+const axios = require('axios');
+const FormData = require('form-data');
 
-const filewUrl = "http://c1741193.ferozo.com/patroncito/public/downloads/cv.docx"; // descargar  desde s3 de amazon
-const tempName = "cv2.docx";
+const apiUrl = "https://docu-edit-demo-api.herokuapp.com/api/file"; // descargar  desde s3 de amazon
+const tempName = "temp-document.docx";
+
+let mainWindow;
+let deeplinkingUrl;
 
 app.setAsDefaultProtocolClient("docuedit");
-// app.setAsDefaultProtocolClient("docuedit", 'C:\\Users\\karthi\\electron-quick-start\\electron-quick-start-win32-x64\\electron-quick-start.exe');
-// this code will register the custom protocol in machine, then you can open your app using browser like quickstart://params
+
+// if (!gotTheLock) {
+//     app.quit();
+//     return;
+// } else {
+    
+//     app.on('second-instance', (e, argv) => {
+//         devToolsLog('second-instance');
+//         if (process.platform !== 'darwin') {
+//             // Find the arg that is our custom protocol url and store it
+//             deeplinkingUrl = argv.find((arg) => arg.startsWith('docuedit://'));
+//         }
+
+//         if (mainWindow) {
+//             if (mainWindow.isMinimized()) mainWindow.restore();
+//             mainWindow.focus();
+//         }
+//     });
+// }
 
 /**
  * Functions
@@ -20,6 +45,7 @@ app.setAsDefaultProtocolClient("docuedit");
 const openFile = async (file) => {
     await open(`./${file}`, { wait: true });
 }
+
 
 const startWatch = (tempName) => {
     let fsWait = false;
@@ -35,6 +61,7 @@ const startWatch = (tempName) => {
         }
     });
 }
+
 
 const createNewWindow = () => {
     let newWindow = new BrowserWindow(
@@ -55,67 +82,64 @@ const createNewWindow = () => {
     });
 }
 
-/**
- * App manage
- */
 
-let templateMenu = [
-    {
-        label: "File",
-        submenu: [
-            {
-                label: "test",
-                click() {
-                    createNewWindow()
-                }
-            },
-            {
-                label: "Load Docx",
-                click() {
-                    http.get(filewUrl, function (response) {
-                        const file = fs.createWriteStream(tempName);
-                        response.pipe(file);
-                        file.on("finish", function () {
-                            file.close();
-                            const watcher = startWatch(tempName);
-                            openFile(tempName).then(() => {
-                                // app.post('/upload', function (req, res) {
-                                //     console.log(req.files.foo); // the uploaded file object
-                                //     console.log("file closed");
-                                //     // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER ANTES DE BORRARLO
-                                //     fs.unlinkSync(`./${tempName}`);
-                                //     watcher.close();
-                                // });
-                                console.log("file closed");
-                                // TODO ACA SE PUEDE ENVIAR EL ARCHIVO TEMPORAL A OTRO SERVER ANTES DE BORRARLO
-                                fs.unlinkSync(`./${tempName}`);
-                                watcher.close();
-                            });
-                        })
-                        file.on("error", function (err) {
-                            console.log("Error to open stream the file");
-                        });
-                    }).on("error", function (e) {
-                        console.log("error to download");
-                    });
-                }
-            }
-        ]
+const devToolsLog = (s) => {
+    if (mainWindow && mainWindow.webContents) {
+        mainWindow.webContents.executeJavaScript(`console.log("send in protocol: ${s}")`);
     }
-];
+}
 
-let mainWindow;
 
-const io = require('socket.io-client');
-const socket = io(`http://localhost:8080`);
+const sendFile = () => {
 
-app.on("ready", () => {
-    
+    http.get(apiUrl, function (response) {
+        const file = fs.createWriteStream(tempName);
+        response.pipe(file);
+        file.on("finish", function () { // cuando termina de cargar el archivo en memoria
+            const watcher = startWatch(tempName); // 
+            openFile(tempName).then( async () => {
+                file.close();
+                //const STATIC_FOLDER = path.join(process.cwd(), tempName);
+                //let fileStream = fs.createReadStream(STATIC_FOLDER);
+                let form = new FormData();
+                const fileStream = fs.readFileSync(`./${tempName}`, {encoding:'utf8', flag:'r'});
+                console.log(fileStream);
+                // form.append('file', fileStream, tempName);
+                // axios.post(apiUrl, {"file": form}, {
+                //     headers: {
+                //         'Content-Type': 'multipart/form-data'
+                //     }
+                // }).then((res) => {
+                //     console.log('file send');
+                //     console.log(res);
+                //     fs.unlinkSync(`./${tempName}`);
+                //     watcher.close();
+                // }, (err) => {
+                //     console.log("ERROR");
+                //     console.log(err);
+                //     fs.unlinkSync(`./${tempName}`);
+                //     watcher.close();
+                // });
+            });
+        });
+        file.on("error", function (err) {
+            console.log("Error to open stream the file");
+        });
+    }).on("error", function (e) {
+        console.log("error to download");
+    });
+
+}
+
+
+function createWindow() {
+
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-            enableRemoteModule: true
+            enableRemoteModule: true,
+            additionalArguments: ["test", "--another=something"]
         }
     });
 
@@ -130,52 +154,104 @@ app.on("ready", () => {
     const mainMenu = Menu.buildFromTemplate(templateMenu);
 
     Menu.setApplicationMenu(mainMenu);
+
     mainWindow.on("closed", () => {
         app.quit();
     });
 
     mainWindow.webContents.openDevTools();
 
-    let isSocketOn = false;
-
-    socket.on('welcome', () => {
-        console.log('on welcome : welcome received renderer');
-        socket.emit('test')
+    const registerHttpProtocol = protocol.registerHttpProtocol('docuedit', (req, cb) => {
+        devToolsLog('registerHttpProtocol');
     });
 
-    socket.on('error', (e) => {
-        console.log(e);
+    const interceptHttpProtocol = protocol.interceptHttpProtocol('docuedit', (req, cb) => {
+        req.url;
+        devToolsLog(req.url);
     });
 
-    socket.on('ok', () => {
-        console.log("OK received renderer");
+    const interceptBufferProtocol = protocol.interceptBufferProtocol('docuedit', (req, cb) => {
+        devToolsLog('interceptBufferProtocol');
     });
 
-    socket.on('connect', function(d) {
-        isSocketOn = true;
-        console.log("conexion exitosa");
-        // socket.emit('test');
-    });
+    proto = {
+        registerHttpProtocol,
+        interceptHttpProtocol,
+        interceptBufferProtocol
+    }
 
-    socket.on('disconnect', () => {
-        console.log("Desconectado");
-    });
+    // ipcMain.on("loaded", (event, data) => {
+    //     event.reply("isConnected", process.argv);
+    // });
 
-    ipcMain.on("loaded", (event, data) => {
-        event.reply("isConnected", isSocketOn);
-    });
+}
 
-    protocol.registerHttpProtocol('docuedit', (req, cb) => {
-        //const fullUrl = formFullTodoUrl(req.url)
-        devToolsLog('full url to open ')
-        // mainWindow.loadURL(fullUrl)
-    });
-    
-    // protocol.registerSchemesAsPrivileged([
-        // { scheme: 'docuedit', privileges: { bypassCSP: true } }
-    // ]);
+/**
+ * App manage
+ */
 
+let templateMenu = [
+    {
+        label: "File",
+        submenu: [
+            {
+                label: "test",
+                click() {
+                    createNewWindow();
+                }
+            },
+            {
+                label: "Load Docx",
+                click() {
+                    sendFile();
+                }
+            }
+        ]
+    }
+];
+
+app.on("ready", createWindow);
+
+
+app.on('second-instance', (e, argv) => {
+    devToolsLog('second-instance');
+    if (process.platform !== 'darwin') {
+        // Find the arg that is our custom protocol url and store it
+        deeplinkingUrl = argv.find((arg) => arg.startsWith('docuedit://'));
+    }
+
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.focus();
+    }
 });
+
+
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+
+// Protocol handler for osx
+app.on('open-url', function (event, url) {
+    event.preventDefault();
+    deeplinkingUrl = url
+    devToolsLog('open-url');
+});
+
 
 if (process.env.NODE_ENV !== 'production') {
     templateMenu.push(
@@ -196,6 +272,7 @@ if (process.env.NODE_ENV !== 'production') {
         }
     );
 }
+
 
 if (process.platform === "darwin") {
     templateMenu.unshift(
